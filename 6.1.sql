30fe30223f5abc9eb3957216aed35f15a42ad84c
vab7 <vab777xyz@gmail.com>
2022-04-27 12:58:35 +0300

    change database

--- 6.1.sql
+++ 6.1.sql
@@ -1,2 +1,41 @@
 use Belousov;
-SELECT * Orders1
+/* in */
+SELECT *
+FROM Customers1
+WHERE snum IN (1001, 1002, 1007);
+/* in */
+SELECT *
+FROM Orders1
+WHERE cnum IN (2001, 2004, 2008);
+/* between */
+SELECT *
+FROM SalesPeople1
+WHERE snum1 BETWEEN 1001 AND 1005;
+/* between */
+SELECT *
+FROM SalesPeople1
+WHERE comm BETWEEN.10 AND.13;
+/* like */
+SELECT *
+FROM Customers1
+WHERE cname LIKE "A%G";
+/* like */
+SELECT *
+FROM Customers1
+WHERE city1 LIKE "A%L";
+/* date */
+SELECT *
+FROM Orders1
+WHERE odate1 BETWEEN "2021.03.10" AND "2021.05.10";
+/* c */
+SELECT *
+FROM Customers1
+WHERE cname LIKE "C%";
+/* g */
+SELECT *
+FROM Customers1
+WHERE cname LIKE "G%";
+/* g and s */
+SELECT *
+FROM Customers1
+WHERE cname LIKE "C%s";
