28f4c09ef40cfd55f139353a2a71d4b93d3e5c13
vab7 <vab777xyz@gmail.com>
2022-04-28 17:00:30 +0300

    8.1

--- 8.1.sql
+++ 8.1.sql
@@ -1,2 +1,42 @@
 USE Belousov;
 /* 1 */
+SELECT cname AS "Заказчик",
+    rating1 AS "Рейтинг"
+FROM Customers1
+ORDER BY rating1 DESC;
+/* 2 */
+SELECT sname1 AS "Продавец",
+    cname AS "Заказчик"
+FROM Customers1
+    JOIN SalesPeople1 ON snum = snum1
+ORDER BY sname1 DESC;
+/* 3 */
+SELECT snum1 AS "Номер",
+    sname1 AS "Имя",
+    city AS "Город"
+FROM SalesPeople1
+ORDER BY sname1 DESC;
+/* 4 */
+SELECT cname AS "Имя",
+    city1 AS "Город",
+    rating1 AS "Рейтинг"
+FROM Customers1
+ORDER BY rating1,
+    cname;
+/* 5 */
+SELECT sname1 AS "Имя",
+    city AS "Город"
+FROM SalesPeople1
+GROUP BY city
+ORDER BY sname1;
+/* 6 */
+SELECT cname AS "Имя",
+    city1 AS "Город",
+    rating1 AS "Рейтинг"
+FROM Customers1
+ORDER BY 2;
+/* 7 */
+SELECT city1 AS "Город",
+    rating1 AS "Рейтинг"
+FROM Customers1
+ORDER BY rating1;
